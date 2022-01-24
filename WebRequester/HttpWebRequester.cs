def8877fe661b9f41820d7b6669c69121dc570e7
yotakih <37173608+yotakih@users.noreply.github.com>
2022-01-25 00:47:45 +0900

    Post機能を追加

--- WebRequester/HttpWebRequester.cs
+++ WebRequester/HttpWebRequester.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -13,39 +14,14 @@ namespace WebRequester
 		#region "static variant"
 		private static HttpClient _client = null;
 		#endregion
-		// #region "Setting Proxy"
-		// /// <summary>
-		// /// 
-		// /// </summary>
-		// public IWebProxy Proxy = null;
-		// /// <summary>
-		// /// 
-		// /// </summary>
-		// /// <param name="proxyurl"></param>
-		// public void SetProxy(string proxyurl)
-		// {
-		// 	this.Proxy = new WebProxy(proxyurl);
-		// }
-		// /// <summary>
-		// /// 
-		// /// </summary>
-		// /// <param name="proxyurl"></param>
-		// /// <param name="authuser"></param>
-		// /// <param name="authpass"></param>
-		// public void SetProxy(string proxyurl, string authuser, string authpass)
-		// {
-		// 	this.SetProxy(proxyurl);
-		// 	var credential = new NetworkCredential(authuser, authpass);
-		// 	this.Proxy.Credentials = credential;
-		// }
-		// #endregion
 		#region "Public Interface"
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <param name="url"></param>
+		/// <param name="header"></param>
 		/// <returns></returns>
-		public async Task<string> Get(string url, string encstr = "utf-8", int timeout = 3000)
+		public async Task<HttpResponseMessage> Get(string url, Dictionary<string, string> header = null)
 		{
 			var client = this.BuildHttpClient();
 			try
@@ -55,7 +31,61 @@ namespace WebRequester
 					Method = HttpMethod.Get,
 					RequestUri = new Uri(url),
 				};
-				var res = await client.SendAsync(msg);
+				if (header is not null)
+					foreach (var k in header.Keys)
+						msg.Headers.Add(k, header[k]);
+				return await client.SendAsync(msg);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"ErrorMessage: {e.Message}");
+				Console.WriteLine($"StackTrace: {e.StackTrace}");
+				throw;
+			}
+			finally
+			{
+				// HttpClientはDisposeせずアプリケーション内で使いまわす仕様
+				// client.Dispose();
+			}
+		}
+		public async Task<HttpResponseMessage> Post(string url, HttpContent postData, Dictionary<string, string> header = null)
+		{
+			var client = this.BuildHttpClient();
+			try
+			{
+				var msg = new HttpRequestMessage
+				{
+					Method = HttpMethod.Post,
+					RequestUri = new Uri(url),
+				};
+				if (header is not null)
+					foreach (var k in header.Keys)
+						msg.Headers.Add(k, header[k]);
+				msg.Content = postData;
+				return await client.SendAsync(msg);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"ErrorMessage: {e.Message}");
+				Console.WriteLine($"StackTrace: {e.StackTrace}");
+				throw;
+			}
+			finally
+			{
+				// HttpClientはDisposeせずアプリケーション内で使いまわす仕様
+				// client.Dispose();
+			}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="res"></param>
+		/// <param name="encstr"></param>
+		/// <returns></returns>
+		public static string ContentFromRes(HttpResponseMessage res, string encstr = "utf-8")
+		{
+			try
+			{
 				var ret = "";
 				using (var st = new StreamReader(res.Content.ReadAsStream(), CommonMdl.GetEncoding(encstr)))
 				{
@@ -71,12 +101,18 @@ namespace WebRequester
 			}
 			finally
 			{
-				// HttpClientはDisposeせずアプリケーション内で使いまわす仕様
-				// client.Dispose();
 			}
+		}
+		public static StringContent ConvPostJsonData(string json, string enc = "utf-8")
+		{
+			return new StringContent(json, CommonMdl.GetEncoding(enc), @"application/json");
 		}
 		#endregion
 		#region "Private"
+		/// <summary>
+		/// HttpClientを生成
+		/// </summary>
+		/// <returns></returns>
 		private HttpClient BuildHttpClient()
 		{
 			if (_client is null)
--- WebRequesterTests/HttpWebRequesterTests.cs
+++ WebRequesterTests/HttpWebRequesterTests.cs
@@ -9,40 +9,41 @@ namespace WebRequesterTests
 	[TestClass]
 	public class WebRequesterTests
 	{
-		// [TestMethod]
-		// public void SetProxyTest()
-		// {
-		// 	var r = new HttpWebRequester();
-		// 	var p = "http://localhost:8080/";
-		// 	var u = "https://www.google.com/?hl=ja";
-		// 	r.SetProxy(p);
-		// 	Assert.AreEqual(p, r.Proxy.GetProxy(new Uri(u)).ToString());
-		// }
-		// [TestMethod]
-		// public void SetProxyTest2()
-		// {
-		// 	var r = new HttpWebRequester();
-		// 	var p = "http://localhost:8080/";
-		// 	var u = new Uri("https://www.google.com/?hl=ja");
-		// 	r.SetProxy(p, "usr", "pass");
-		// 	var c = r.Proxy.Credentials.GetCredential(u, "Basic");
-		// 	Assert.IsTrue("usr" == c.UserName && "pass" == c.Password);
-		// }
-
 		[TestMethod]
 		public void GetTest()
 		{
 			var r = new HttpWebRequester();
-			var u = "https://www.google.com/?hl=ja";
+			var u = "http://localhost:3000";
 			var c = r.Get(u).Result;
-			Assert.IsTrue(c.Length > 0);
+			Assert.AreEqual(c.StatusCode, HttpStatusCode.OK);
+		}
+		[TestMethod]
+		public void PostTest()
+		{
+			var url = "http://localhost:3000";
+			var data = HttpWebRequester.ConvPostJsonData(@"{ ""hoge"": ""ほげ"" }");
+			Console.WriteLine(data);
+			var req = new HttpWebRequester();
+			var res = req.Post(url, data).Result;
+			Console.WriteLine(HttpWebRequester.ContentFromRes(res));
+			Assert.AreEqual(res.StatusCode, HttpStatusCode.OK);
+		}
+		[TestMethod]
+		public void ContentFromResTest()
+		{
+			var req = new HttpWebRequester();
+			var url = "http://www.google.com/?hl=ja";
+			var res = req.Get(url).Result;
+			var s = HttpWebRequester.ContentFromRes(res, "utf-8");
+			Assert.IsTrue(s.Length > 0);
 		}
 		[TestMethod]
 		public void GetBadUrlTest()
 		{
 			var r = new HttpWebRequester();
 			var u = "http://192.168.1.21:5555";
-			Assert.ThrowsException<Exception>(() => {
+			Assert.ThrowsException<Exception>(() =>
+			{
 				var c = r.Get(u).Result;
 			});
 		}
